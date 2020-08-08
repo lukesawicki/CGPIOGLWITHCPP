72de35fe83a4bd0c62ccd617748fb2fd7d8d6908
lukesawicki <lukaszsa1@gmail.com>
2020-08-08 13:10:10 +0200

    Trying do rotating and moving cube.

--- CGPIOGLWITHCPP/fragShader.glsl
+++ CGPIOGLWITHCPP/fragShader.glsl
@@ -7,8 +7,7 @@ out vec4 color;
 uniform mat4 mv_matrix;
 uniform mat4 proj_matrix;
 
-
-
 void main(void)
-{	color = varyingColor;
+{
+	color = varyingColor;
 }
--- CGPIOGLWITHCPP/main.cpp
+++ CGPIOGLWITHCPP/main.cpp
@@ -96,23 +96,31 @@ void display(GLFWwindow* window, double currentTime)
     aspect = static_cast<float>(width / height);
     
     perspectiveMatrix = glm::perspective(1.0472F, aspect, 0.1F, 1000.0F); // 1.0472 radians = 60 degrees
-    std::cout << (int)&perspectiveMatrix << std::endl;
+    //std::cout << (int)&perspectiveMatrix << std::endl;
     viewMatrix = glm::translate(glm::mat4(1.0F), glm::vec3(-cameraX, -cameraY, -cameraZ));
     
     // modelMatrix = glm::translate(glm::mat4(1.0F), glm::vec3(cubeLocationX, cubeLocationY, cubeLocationZ));
-    //translationModelMatrix = glm::translate(glm::mat4(1.0F), glm::vec3(sin(0.35F*currentTime)*2.0F, cos(0.52F*currentTime)*2.0F, sin(0.7F*currentTime)*2.0F));
+    
+//translationModelMatrix = glm::translate(glm::mat4(1.0F), glm::vec3(sin(0.35F*currentTime)*2.0F, cos(0.52F*currentTime)*2.0F, sin(0.7F*currentTime)*2.0F));
+    translationModelMatrix = glm::translate(glm::mat4(1.0F), glm::vec3(1.0F, 1.0F, 1.0F));
     
     //rotationModelMatrix = glm::rotate(glm::mat4(1.0F), 1.75F*static_cast<float>(currentTime), glm::vec3(0.0F, 1.0F, 0.0F));
     //rotationModelMatrix = glm::rotate(rotationModelMatrix, 1.75F*static_cast<float>(currentTime), glm::vec3(1.0F, 0.0F, 0.0F));
-    rotationModelMatrix = glm::rotate(glm::mat4(1.0F), 1.75F*static_cast<float>(currentTime), glm::vec3(0.0F, 0.0F, 1.0F));
+    
+    ///rotationModelMatrix = glm::rotate(glm::mat4(1.0F), 1.75F*static_cast<float>(currentTime), glm::vec3(0.0F, 0.0F, 1.0F));
 
     
-    //rotationModelMatrix = glm::rotate(glm::mat4(1.0F), 1.75F*static_cast<float>(currentTime), glm::vec3(0.0F, 1.0F, 0.0F));
-    //rotationModelMatrix = glm::rotate(rotationModelMatrix, 1.75F*static_cast<float>(currentTime), glm::vec3(1.0F, 0.0F, 0.0F));
-    //rotationModelMatrix = glm::rotate(rotationModelMatrix, 1.75F*static_cast<float>(currentTime), glm::vec3(0.0F, 0.0F, 1.0F));
+//rotationModelMatrix = glm::rotate(glm::mat4(1.0F), 1.75F*static_cast<float>(currentTime), glm::vec3(0.0F, 1.0F, 0.0F));
+//rotationModelMatrix = glm::rotate(rotationModelMatrix, 1.75F*static_cast<float>(currentTime), glm::vec3(1.0F, 0.0F, 0.0F));
+//rotationModelMatrix = glm::rotate(rotationModelMatrix, 1.75F*static_cast<float>(currentTime), glm::vec3(0.0F, 0.0F, 1.0F));
+
+    rotationModelMatrix = glm::rotate(glm::mat4(1.0F), 1.75F*static_cast<float>(currentTime), glm::vec3(1.0F, 0.0F, 0.0F));
+    rotationModelMatrix = glm::rotate(rotationModelMatrix, 1.75F*static_cast<float>(currentTime), glm::vec3(0.0F, 1.0F, 0.0F));
+    rotationModelMatrix = glm::rotate(rotationModelMatrix, 1.75F*static_cast<float>(currentTime), glm::vec3(0.0F, 0.0F, 1.0F));
 
    	//rMat = glm::rotate(rotationModelMatrix, 1.75f*(float)currentTime, glm::vec3(1.0f, 0.0f, 0.0f));
 	//rMat = glm::rotate(rMat, 1.75f*(float)currentTime, glm::vec3(0.0f, 0.0f, 1.0f));
+
     modelMatrix = translationModelMatrix * rotationModelMatrix;
 
     modelViewMatrix = viewMatrix * modelMatrix;
